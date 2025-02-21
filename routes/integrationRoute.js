fe3d36d5dc1fcfbde15f2d0df7b047748f3673ac
Awesome6192 <christianchibuike14@gmail.com>
2025-02-21 18:34:20 +0100

    feat: Add route for Telex integration in integrationRoute.js

---
+++ routes/integrationRoute.js
@@ -0,0 +1,12 @@
+// Import the express module
+const express = require("express");
+// Initialize a new router object for defining routes
+const router = express.Router();
+// Import the controller function to handle the project and task data retrieval
+const { integrationJson } = require('../controllers/integrationController');
+
+// Route for telex integration
+router.route("/").get(integrationJson);
+
+// Export router
+module.exports = router;
